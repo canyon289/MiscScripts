6c931143de9d9022064fd2f11d9edd049a560306
canyon289 <ravinsdrive@gmail.com>
2014-05-21 09:48:54 -0500

    Adding questions file

---
+++ r/Questions.r
@@ -0,0 +1,9 @@
+#Learn the difference between these two
+library(datasets)
+data(iris)
+x = iris["Species"]
+y = iris$Species
+
+
+#How does he call the $ right after tapply
+#Reference slide 5 of t apply lecture
